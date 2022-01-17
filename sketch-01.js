da408aed4cc5d92da209ab0d45ec5fbc1a1417bc
vernekarakshata <akshatav10@gmail.com>
2022-01-17 21:36:36 +0530

    modified sketch-01

--- sketch-01.js
+++ sketch-01.js
@@ -1,41 +1,65 @@
 const canvasSketch = require('canvas-sketch');
+const Tweakpane = require('tweakpane');
 
 const settings = {
-  dimensions: [600, 600]
+  dimensions: [1080, 1080],
+  animate: true
 };
 
-const sketch = () => {
+const params = {
+  invert: false,
+  speed: 30
+}
+
+const sketch = ({ width, height }) => {
+  let steps = 0;
+  let x, y;
+  const w = width * 0.10, h = height * 0.10;
+  const ix = width * 0.17, iy = height * 0.17;
+  const gap = width * 0.03, off = width * 0.02;
+
   return ({ context, width, height }) => {
     context.lineWidth = width * 0.01;
+    steps++;
 
-    // context.fillStyle = "#000000";
-    // context.fillRect(0, 0, width, height);
-    // context.strokeStyle = "#FFFFFF"
+    if (steps === params.speed) {
+      context.clearRect(0, 0, width, height);
 
-    let x, y;
-    const w = width * 0.10, h = height * 0.10;
-    const ix = width * 0.17, iy = height * 0.17;
-    const gap = width * 0.03, off = width * 0.02;
-
-    for (let i = 0; i < 5; i++) {
-
-      for (let j = 0; j < 5; j++) {
-        x = ix + (w + gap) * i;
-        y = iy + (h + gap) * j;
-        context.beginPath();
-        context.rect(x, y, w, h);
-        context.stroke();
-
-        if (Math.random() > 0.5) {
-          context.beginPath();
-          context.rect(x + off / 2, y + off / 2, w - off, h - off);
-          context.stroke();
-        }
+      if (params.invert) {
+        context.fillStyle = "#000000";
+        context.fillRect(0, 0, width, height);
+        context.strokeStyle = "#FFFFFF"
       }
 
-    }
+      for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 5; j++) {
+          x = ix + (w + gap) * i;
+          y = iy + (h + gap) * j;
+          drawRect(context, x, y, w, h);
 
+          if (Math.random() > 0.5) {
+            drawRect(context, x + off / 2, y + off / 2, w - off, h - off);
+          }
+        }
+      }
+      steps = 0;
+    }
   };
 };
+
+const drawRect = (context, x, y, w, h) => {
+  context.beginPath();
+  context.rect(x, y, w, h);
+  context.stroke();
+}
+const createPane = () => {
+  const pane = new Tweakpane.Pane();
+  let folder;
+  folder = pane.addFolder({
+    title: 'Square-Sketch',
+  });
+  folder.addInput(params, 'invert');
+}
+createPane();
 
 canvasSketch(sketch, settings);

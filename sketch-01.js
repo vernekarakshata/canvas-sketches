c45728357c1863475af02e4a32f84b4e2c12b4e6
vernekarakshata <vernekarakshata@outlook.com>
2024-10-21 23:07:38 +0530

    modify changes

--- sketch-01.js
+++ sketch-01.js
@@ -3,63 +3,70 @@ const Tweakpane = require('tweakpane');
 
 const settings = {
   dimensions: [1080, 1080],
-  animate: true
+  animate: true,
+  fps: 1.2,
+  playbackRate: 'throttle',
 };
 
 const params = {
-  invert: false,
-  speed: 30
-}
+  stroke: '#000000',
+  background: '#ffffff',
+  lineWidth: 0.01,
+};
 
-const sketch = ({ width, height }) => {
-  let steps = 0;
-  let x, y;
-  const w = width * 0.10, h = height * 0.10;
-  const ix = width * 0.17, iy = height * 0.17;
-  const gap = width * 0.03, off = width * 0.02;
-
+const sketch = () => {
   return ({ context, width, height }) => {
-    context.lineWidth = width * 0.01;
-    steps++;
+    context.fillStyle = params.background;
+    context.fillRect(0, 0, width, height);
+    context.lineWidth = width * params.lineWidth;
 
-    if (steps === params.speed) {
-      context.clearRect(0, 0, width, height);
+    const w = width * 0.1;
+    const h = height * 0.1;
+    const gap = width * 0.03;
 
-      if (params.invert) {
-        context.fillStyle = "#000000";
-        context.fillRect(0, 0, width, height);
-        context.strokeStyle = "#FFFFFF"
-      }
+    const ix = width * 0.17;
+    const iy = height * 0.17;
 
-      for (let i = 0; i < 5; i++) {
-        for (let j = 0; j < 5; j++) {
-          x = ix + (w + gap) * i;
-          y = iy + (h + gap) * j;
-          drawRect(context, x, y, w, h);
+    const off = width * 0.02;
 
-          if (Math.random() > 0.5) {
-            drawRect(context, x + off / 2, y + off / 2, w - off, h - off);
-          }
+    context.strokeStyle = params.stroke;
+
+    for (let i = 0; i < 5; i++) {
+      for (let j = 0; j < 5; j++) {
+        x = ix + (w + gap) * i;
+        y = iy + (h + gap) * j;
+
+        context.beginPath();
+        context.rect(x, y, w, h);
+        context.stroke();
+
+        if (Math.random() > 0.5) {
+          context.beginPath();
+          context.rect(x + off / 2, y + off / 2, w - off, h - off);
+          context.stroke();
         }
       }
-      steps = 0;
     }
   };
 };
 
-const drawRect = (context, x, y, w, h) => {
-  context.beginPath();
-  context.rect(x, y, w, h);
-  context.stroke();
-}
 const createPane = () => {
   const pane = new Tweakpane.Pane();
   let folder;
-  folder = pane.addFolder({
-    title: 'Square-Sketch',
+
+  folder = pane.addFolder({ title: 'Properties' });
+  pane.addInput(params, 'stroke', {
+    picker: 'inline',
+    expanded: false,
   });
-  folder.addInput(params, 'invert');
-}
+  pane.addInput(params, 'background', {
+    picker: 'inline',
+    expanded: false,
+  });
+
+  folder.addInput(params, 'lineWidth', { min: 0.001, max: 0.02, step: 0.001 });
+};
+
 createPane();
 
 canvasSketch(sketch, settings);
--- sketch-02.js
+++ sketch-02.js
@@ -1,9 +1,28 @@
 const canvasSketch = require('canvas-sketch');
 const math = require('canvas-sketch-util/math');
 const random = require('canvas-sketch-util/random');
+const Tweakpane = require('tweakpane');
+
+const lineParams = {
+  width: 5
+};
+
+const stripParams = {
+  count: 10,
+  height: 1,
+  width: 2
+};
+
+const arcParams = {
+  sradius: 0.2,
+  eradius: 1.5
+};
 
 const settings = {
-  dimensions: [1080, 1080]
+  dimensions: [1080, 1080],
+  animate: true,
+  fps: 5,
+  playbackRate: 'throttle'
 };
 
 const sketch = () => {
@@ -13,6 +32,7 @@ const sketch = () => {
 
     context.fillStyle = 'black';
 
+    // Center Point
     const cx = width * 0.5;
     const cy = height * 0.5;
 
@@ -20,7 +40,7 @@ const sketch = () => {
     const h = height * 0.1;
     let x, y;
 
-    const num = 40;
+    const num = stripParams.count;
     const radius = width * 0.3;
 
     for (let i = 0; i < num; i++) {
@@ -29,31 +49,59 @@ const sketch = () => {
 
       x = cx + radius * Math.sin(angle);
       y = cy + radius * Math.cos(angle);
-      context.save();
-      context.translate(x, y);
-      context.rotate(-angle);
-      context.scale(random.range(0.1, 2), random.range(0.2, 0.5));
-      context.beginPath();
-      context.rect(-w * 0.5, random.range(0, -h * 0.5), w, h);
-      context.fill();
-      context.restore();
 
-      context.save();
-      context.translate(cx, cy);
-      context.rotate(-angle);
-      context.lineWidth = random.range(5, 20);
-
-      context.beginPath();
-      context.arc(0, 0, radius * random.range(0.7, 1.3), slice * random.range(1, -8), slice * random.range(1, 5));
-      context.stroke();
-      context.restore();
+      drawRect(context, x, y, w, h, angle);
+      drawArc(context, cx, cy, radius, angle, slice);
     }
-
-
-
-
-
   };
 };
+
+const drawRect = (context, x, y, w, h, angle) => {
+  context.save();
+  context.translate(x, y);
+  context.rotate(-angle);
+  context.scale(
+    random.range(0.1, stripParams.width),
+    random.range(0.2, stripParams.height)
+  );
+
+  context.beginPath();
+  context.rect(-w * 0.5, random.range(0, -h * 0.5), w, h);
+  context.fill();
+  context.restore();
+};
+
+const drawArc = (context, cx, cy, r, angle, slice) => {
+  context.save();
+  context.translate(cx, cy);
+  context.rotate(-angle);
+  context.lineWidth = random.range(1, lineParams.width);
+  context.beginPath();
+  const arcRadius = r * random.range(arcParams.sradius, arcParams.eradius);
+  const sAngle = slice * random.range(1, -8);
+  const eAngle = slice * random.range(1, 5);
+  context.arc(0, 0, arcRadius, sAngle, eAngle);
+  context.stroke();
+  context.restore();
+};
+
+const createPane = () => {
+  const pane = new Tweakpane.Pane();
+  let folder;
+
+  folder = pane.addFolder({ title: 'Strip' });
+  folder.addInput(stripParams, 'count', { min: 2, max: 1000, step: 1 });
+  folder.addInput(stripParams, 'width', { min: 1, max: 10, step: 0.01 });
+  folder.addInput(stripParams, 'height', { min: 0.1, max: 2, step: 0.01 });
+
+  folder = pane.addFolder({ title: 'Arc' });
+  folder.addInput(arcParams, 'sradius', { min: 0, max: 0.5, step: 0.001 });
+  folder.addInput(arcParams, 'eradius', { min: 0.8, max: 1.5, step: 0.001 });
+
+  folder = pane.addFolder({ title: 'Line' });
+  folder.addInput(lineParams, 'width', { min: 0, max: 20, step: 1 });
+};
+
+createPane();
 
 canvasSketch(sketch, settings);
